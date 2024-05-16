69ea8f09b1aa6491c3f99190549d6f70eccb925b
Alejolora25 <alejandroloratovar@outlook.com>
2024-05-16 18:26:27 -0500

    se crean font de administrador categoria y usuario2

---
+++ src/components/CardManageCategory.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const CardManageCategory = ({ category }) => {
+    return (
+        <div className='row'>
+            <div className="col-10 card-category ">
+                <div className="card-body  d-flex flex-row align-items-center">
+                    <h4 className="col card-title mx-1">{category.id}</h4>
+                    <p className="col card-text mx-1">{category.name_category}</p>
+                </div>
+            </div>
+            <div className='col-2 d-flex align-items-center'>
+                <a href="#" className="btn btn-primary">Delete</a>
+                <a href="#" className="btn btn-primary mx-1">Edit</a>
+            </div>
+        </div >
+    )
+}
--- src/components/ListManageCategory.jsx
+++ src/components/ListManageCategory.jsx
@@ -1,30 +1,31 @@
-import React, { useState, useEffect } from 'react'
-import * as categoryService from '../services/CategoryService'
-import { CardManageCategory } from './CardManageCategory';
+import React, { useEffect, useState } from "react";
+import { CardManageCategory } from "./CardManageCategory";
+import * as categoryService from "../services/CategoryService";
 
 const ListManageCategory = () => {
-  const [categories, setCategories]= useState([]);
 
-  const getCategory=async ()=>{
-    const data= await categoryService.getCategoriesService();
-    setCategories(data.categories)
-  }
+  const [categories, setCategories] = useState([]);
+
+  const getcategories = async () => {
+    const data = await categoryService.getCategoriesService();
+    setCategories(data.categories);
+  };
 
   useEffect(() => {
-    getCategory();
+    getcategories();
   }, []);
 
-  return (
-    <div>
-      <div className="d-flex flex-column mt-3">
-        {categories?.map((category) => (
-          <div key={category.id} className="mb-4">
-            <CardManageCategory key={category.id} category={category} />
-          </div>
-        ))}
-      </div>
+return (
+  <div>
+    <div className="d-flex flex-column mt-3">
+      {categories?.map((user) => (
+        <div key={user.id} className="mb-4">
+          <CardManageCategory key={user.id} user={user} />
+        </div>
+      ))}
     </div>
-  )
-}
+  </div>
+);
+};
 
 export default ListManageCategory
