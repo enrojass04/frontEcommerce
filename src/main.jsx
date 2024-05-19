b2fa2403a007fd9655635647016458e7190eac3a
Alejolora25 <alejandroloratovar@outlook.com>
2024-05-19 17:15:11 -0500

    se crea crea carrito, se crea contexto para carrito se modifica main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { CartProvider } from '../src/components/Cart/CartContext.jsx'; // Importa el CartProvider
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+  <React.StrictMode>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </React.StrictMode>
+);
