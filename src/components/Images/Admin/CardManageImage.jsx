cf1103f44ae720b3a4b4ae01acb0c81eef85c7ca
Alejolora25 <alejandroloratovar@outlook.com>
2024-05-21 17:41:36 -0500

    se modifica crud de imagenes para que se muestre imagen

--- src/components/Images/Admin/CardManageImage.jsx
+++ src/components/Images/Admin/CardManageImage.jsx
@@ -6,13 +6,12 @@ export const CardManageImage = ({ image }) => {
       <div className="col-10 card-image ">
         <div className="card-body  d-flex flex-row align-items-center">
           <img
-            src={image.url_image}
+            src={`data:image/png;base64, ${image.url_image}`}
             alt="thumbnail"
             className="img-thumbnail mx-1"
             style={{ width: "100px", height: "100px" }}
           />
           <h4 className="col card-title mx-1">{image.id}</h4>
-          <p className="col card-text mx-1">{image.url_image}</p>
         </div>
       </div>
       <div className="col-2 d-flex align-items-center">
