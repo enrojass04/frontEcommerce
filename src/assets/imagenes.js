1c274ebe797105001ad30c7584c5e9b311ca0df0
Manuel Chicangana <jmanuelch17@hotmail.com>
2024-05-23 17:53:13 -0500

    se hace el tema de categorias en la parte de sola maquetación

--- src/assets/imagenes.js
+++ src/assets/imagenes.js
@@ -1,47 +1,57 @@
-import image1 from './img/image 1.png'
-import logo from './logo/Meubel House_Logos-05.png'
-import fondo from './img/scandinavian-interior-mockup-wall-decal-background 1.png'
-import avatar from './img/avatar foto.jpg'
-import logoEma from './logo/LogoEMA-green.svg'
-import logoSinFond from './logo/LogoEMA-green.svg'
-import creditCard from './img/CreditCard.png'
-import imagenProducto1 from '../assets/img/producto1.jpg';
-import imagenProducto2 from '../assets/img/producto2.jpg';
-import imagenProducto3 from '../assets/img/producto3.jpg';
-import imagenProducto4 from '../assets/img/producto4.jpg';
-import imagenProducto5 from '../assets/img/producto5.jpg';
-import imagenProducto6 from '../assets/img/producto6.jpg';
-import imagenProducto7 from '../assets/img/producto7.jpg';
-import imagenProducto8 from '../assets/img/producto8.jpg';
-import imagenProducto9 from '../assets/img/producto9.jpg';
-import imagenProducto10 from '../assets/img/producto10.jpg';
-import imagenProducto11 from '../assets/img/producto11.jpg';
-import imagenProducto12 from '../assets/img/producto12.jpg';
-import imagenProducto13 from '../assets/img/producto13.jpg';
-import imagenProducto14 from '../assets/img/producto14.jpg';
+import image1 from "./img/image 1.png";
+import logo from "./logo/Meubel House_Logos-05.png";
+import fondo from "./img/scandinavian-interior-mockup-wall-decal-background 1.png";
+import avatar from "./img/avatar foto.jpg";
+import logoEma from "./logo/LogoEMA-green.svg";
+import logoSinFond from "./logo/LogoEMA-green.svg";
+import creditCard from "./img/CreditCard.png";
+import imagenProducto1 from "../assets/img/producto1.jpg";
+import imagenProducto2 from "../assets/img/producto2.jpg";
+import imagenProducto3 from "../assets/img/producto3.jpg";
+import imagenProducto4 from "../assets/img/producto4.jpg";
+import imagenProducto5 from "../assets/img/producto5.jpg";
+import imagenProducto6 from "../assets/img/producto6.jpg";
+import imagenProducto7 from "../assets/img/producto7.jpg";
+import imagenProducto8 from "../assets/img/producto8.jpg";
+import imagenProducto9 from "../assets/img/producto9.jpg";
+import imagenProducto10 from "../assets/img/producto10.jpg";
+import imagenProducto11 from "../assets/img/producto11.jpg";
+import imagenProducto12 from "../assets/img/producto12.jpg";
+import imagenProducto13 from "../assets/img/producto13.jpg";
+import imagenProducto14 from "../assets/img/producto14.jpg";
+import catCelulares from "../assets/img/celulares.png";
+import catComputadores from "../assets/img/computadores.png";
+import catTelevisores from "../assets/img/televisores.png";
+import catConsolas from "../assets/img/consolas.png";
+import catReloges from "../assets/img/relojes.png";
+import catCamaras from "../assets/img/camaras.png";
 
 export default {
-    "img1": image1,
-    "logo": logo,
-    "banner": fondo,
-    "avatar": avatar,
-    "logoEMA": logoEma,
-    "logoSinFond": logoSinFond,
-    "creditCard": creditCard,
-    "imgpro1":imagenProducto1,
-    "imgpro2":imagenProducto2,
-    "imgpro3":imagenProducto3,
-    "imgpro4":imagenProducto4,
-    "imgpro5":imagenProducto5,
-    "imgpro6":imagenProducto6,
-    "imgpro7":imagenProducto7,
-    "imgpro8":imagenProducto8,
-    "imgpro9":imagenProducto9,
-    "imgpro10":imagenProducto10,
-    "imgpro11":imagenProducto11,
-    "imgpro12":imagenProducto12,
-    "imgpro13":imagenProducto13,
-    "imgpro14":imagenProducto14,
-
-}
-
+  img1: image1,
+  logo: logo,
+  banner: fondo,
+  avatar: avatar,
+  logoEMA: logoEma,
+  logoSinFond: logoSinFond,
+  creditCard: creditCard,
+  imgpro1: imagenProducto1,
+  imgpro2: imagenProducto2,
+  imgpro3: imagenProducto3,
+  imgpro4: imagenProducto4,
+  imgpro5: imagenProducto5,
+  imgpro6: imagenProducto6,
+  imgpro7: imagenProducto7,
+  imgpro8: imagenProducto8,
+  imgpro9: imagenProducto9,
+  imgpro10: imagenProducto10,
+  imgpro11: imagenProducto11,
+  imgpro12: imagenProducto12,
+  imgpro13: imagenProducto13,
+  imgpro14: imagenProducto14,
+  catCelulares: catCelulares,
+  catComputadores: catComputadores,
+  catTelevisores: catTelevisores,
+  catConsolas: catConsolas,
+  catReloges: catReloges,
+  catCamaras: catCamaras,
+};
--- src/pages/User/HomeUser.jsx
+++ src/pages/User/HomeUser.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "../../App.css";
+import imagenes from "../../assets/imagenes";
 
 export const HomeUser = () => {
   return (
@@ -19,6 +20,43 @@ export const HomeUser = () => {
           <div className="slide slide1"></div>
           <div className="slide slide2"></div>
           <div className="slide slide3"></div>
+        </div>
+        <div className="categorias">
+          <h2>
+            Nuestras mejores <span>categorias</span>
+          </h2>
+          <div className="grid-categorias">
+            <div className="cat1 row justify-content-center">
+              <img src={imagenes.catCelulares} alt={imagenes.catCelulares} />
+              <span>Celulares</span>
+            </div>
+            <div className="cat2 row justify-content-center">
+              <img
+                src={imagenes.catComputadores}
+                alt={imagenes.catComputadores}
+              />
+              <span>Computadores</span>
+            </div>
+            <div className="cat3 row justify-content-center">
+              <img
+                src={imagenes.catTelevisores}
+                alt={imagenes.catTelevisores}
+              />
+              <span>Televisores</span>
+            </div>
+            <div className="cat4 row justify-content-center">
+              <img src={imagenes.catConsolas} alt={imagenes.catConsolas} />
+              <span>Consolas</span>
+            </div>
+            <div className="cat5 row justify-content-center">
+              <img src={imagenes.catReloges} alt={imagenes.catReloges} />
+              <span>Reloges</span>
+            </div>
+            <div className="cat6 row justify-content-center">
+              <img src={imagenes.catCamaras} alt={imagenes.catCamaras} />
+              <span>Camaras</span>
+            </div>
+          </div>
         </div>
       </section>
     </>
