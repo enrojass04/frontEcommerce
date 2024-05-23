699e42a727be04f16c625024a13ecdd846a3202d
Manuel Chicangana <jmanuelch17@hotmail.com>
2024-05-23 14:41:25 -0500

    se hace cambio del banner principal y estilos para presentación

--- src/pages/User/HomeUser.jsx
+++ src/pages/User/HomeUser.jsx
@@ -2,21 +2,25 @@ import React, { useState } from "react";
 import "../../App.css";
 
 export const HomeUser = () => {
-
   return (
     <>
-      <section className="banner">
-        <div className="info-banner">
-          <p className="fw-bold text-start">Llega lo nuevo</p>
-          <h1 className="title-banner fw-bold text-start">
-            Descubre nuestra<br></br>nueva colección
-          </h1>
-          <p className="text-start">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
-            tellus, luctus nec ullamcorper mattis.
-          </p>
-        </div>             
-      </section>      
+      <section className="section-banner">
+        <div className="banner">
+          <div className="info-banner">
+            <span className="fw-bold text-start">Llega lo nuevo</span>
+            <h1 className="title-banner fw-bold text-start">
+              Descubre nuestra<br></br>nueva colección
+            </h1>
+            <p className="text-start">
+              Descubre nuestra nueva colección de tecnología avanzada.
+              ¡Innovación y calidad en cada producto, no te lo pierdas!
+            </p>
+          </div>
+          <div className="slide slide1"></div>
+          <div className="slide slide2"></div>
+          <div className="slide slide3"></div>
+        </div>
+      </section>
     </>
   );
 };
